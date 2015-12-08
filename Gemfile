dd9f5f17204b48f22f87b9a0128cd1d4def6084e
Sam Vredenburgh <sam.vburgh@gmail.com>
2015-12-08 10:43:47 -0600

    setting up db

--- Gemfile
+++ Gemfile
@@ -10,3 +10,4 @@ gem 'activerecord'
 gem 'activesupport'
 gem 'bcrypt'
 gem 'rake'
+gem "dotenv"
---
+++ db/seed.sql
@@ -0,0 +1,46 @@
+INSERT INTO mountains (mountain_lat, mountain_long, mountain_name, mountain_city, mountain_state, mountain_url)
+VALUES (44.1069, -72.8581, 'Sugarbush Resort', 'Warren', 'VT', 'https://www.sugarbush.com/');
+
+INSERT INTO mountains (mountain_lat, mountain_long, mountain_name, mountain_city, mountain_state, mountain_url)
+VALUES ();
+
+INSERT INTO mountains (mountain_lat, mountain_long, mountain_name, mountain_city, mountain_state, mountain_url)
+VALUES ();
+
+INSERT INTO mountains (mountain_lat, mountain_long, mountain_name, mountain_city, mountain_state, mountain_url)
+VALUES ();
+
+INSERT INTO mountains (mountain_lat, mountain_long, mountain_name, mountain_city, mountain_state, mountain_url)
+VALUES ();
+
+INSERT INTO mountains (mountain_lat, mountain_long, mountain_name, mountain_city, mountain_state, mountain_url)
+VALUES ();
+
+INSERT INTO mountains (mountain_lat, mountain_long, mountain_name, mountain_city, mountain_state, mountain_url)
+VALUES ();
+
+INSERT INTO mountains (mountain_lat, mountain_long, mountain_name, mountain_city, mountain_state, mountain_url)
+VALUES ();
+
+INSERT INTO mountains (mountain_lat, mountain_long, mountain_name, mountain_city, mountain_state, mountain_url)
+VALUES ();
+
+INSERT INTO mountains (mountain_lat, mountain_long, mountain_name, mountain_city, mountain_state, mountain_url)
+VALUES ();
+
+INSERT INTO mountains (mountain_lat, mountain_long, mountain_name, mountain_city, mountain_state, mountain_url)
+VALUES ();
+
+INSERT INTO mountains (mountain_lat, mountain_long, mountain_name, mountain_city, mountain_state, mountain_url)
+VALUES ();
+
+INSERT INTO mountains (mountain_lat, mountain_long, mountain_name, mountain_city, mountain_state, mountain_url)
+VALUES ();
+
+INSERT INTO mountains (mountain_lat, mountain_long, mountain_name, mountain_city, mountain_state, mountain_url)
+VALUES ();
+
+INSERT INTO mountains (mountain_lat, mountain_long, mountain_name, mountain_city, mountain_state, mountain_url)
+VALUES ();
+
+SELECT * FROM mountains;
